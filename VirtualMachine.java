0840bc694900ef1059253e8e5739e9e3135ab859
Felipe  Elsner Silva <felipeelsnersilva@MacBook-Pro-de-Elsner.local>
2023-03-29 19:48:52 -0300

    feat: creat menu structure

--- VirtualMachine.java
+++ VirtualMachine.java
@@ -1,7 +1,48 @@
+import java.util.Scanner;
+
 public class VirtualMachine {
 	private static Cpu cpu;
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(system.in);
+		boolean execVM = true;
+		do{
+			String entrada = in.next();
+			String[] vetEntr = entrada.split(" ");
+			switch(vetEntr[0]){
+				case "cria":
+					break;
+
+				case "dump":
+					break;
+
+				case "listaProcessos":
+					break;
+
+				case "desaloca":
+					break;
+
+				case "dumpM":
+					break;
+
+				case "executa":
+					break;
+
+				case "traceOn":
+					break;
+
+				case "traceOff":
+					break;
+					
+				case "exit":
+					execVM = false;
+					break;
+
+				default:
+					break;
+			}
+
+		} while(execVM)
 	}
 }
 

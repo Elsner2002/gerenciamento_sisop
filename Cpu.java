070ac6c05b19dead8fe265557082010133ffbf7d
Bernardozomer <bernardobarzottoz@gmail.com>
2023-04-23 11:03:42 -0300

    feat: add initial code for CPU cycle

--- Cpu.java
+++ Cpu.java
@@ -2,8 +2,185 @@ public class Cpu {
 	public static final int MIN_INT = -32767;
 	public static final int MAX_INT = 32767;
 	private CpuState state;
+	private Memory memory;
+	private InterruptHandler interruptHandler;
+	private SyscallHandler syscallHandler;
 
-	private boolean testForOverflow(int v) {
+	public void run() {
+		while (true) {
+			fetch();
+			execute();
+			interrupt();
+		}
+	}
+
+	private void fetch() {
+		if (!isLegalAddress(this.state.getPc())) {
+			return;
+		}
+
+		// TODO
+		this.state.setIr(null);
+	}
+
+	private void execute() {
+		Word ir = this.state.getIr();
+		int r1 = this.state.getReg(ir.r1());
+		int r2 = this.state.getReg(ir.r2());
+		int param = this.state.getReg(ir.param());
+		boolean jump = false;
+
+		switch (this.state.getIr().opcode()) {
+			case ADD:
+				int sum = r1 + r2;
+
+				if (!isOverflow(sum)) {
+					this.state.setReg(r1, sum);
+				}
+
+				break;
+			case ADDI:
+				int sumI = r1 + ir.param();
+
+				if (!isOverflow(sumI)) {
+					this.state.setReg(r1, sumI);
+				}
+
+				break;
+			case JMP:
+				this.state.setPc(ir.param());
+				jump = true;
+				break;
+			case JMPI:
+				this.state.setPc(ir.r1());
+				jump = true;
+				break;
+			case JMPIE:
+				if (r2 == 0) {
+					this.state.setPc(r1);
+					jump = true;
+				}
+
+				break;
+			case JMPIEK:
+				if (r2 == 0) {
+					this.state.setPc(param);
+					jump = true;
+				}
+
+				break;
+			// TODO.
+			case JMPIEM:
+				break;
+			case JMPIG:
+				if (r2 > 0) {
+					this.state.setPc(r1);
+					jump = true;
+				}
+
+				break;
+			case JMPIGK:
+				if (r2 > 0) {
+					this.state.setPc(param);
+					jump = true;
+				}
+
+				break;
+			// TODO.
+			case JMPIGM:
+				break;
+			case JMPIGT:
+				if (r1 > r2) {
+					this.state.setPc(param);
+					jump = true;
+				}
+
+				break;
+			case JMPIL:
+				if (r2 < 0) {
+					this.state.setPc(r1);
+					jump = true;
+				}
+
+				break;
+			case JMPILK:
+				if (r2 < 0) {
+					this.state.setPc(param);
+					jump = true;
+				}
+
+				break;
+			// TODO.
+			case JMPILM:
+				break;
+			// TODO.
+			case JMPIM:
+				break;
+			// TODO.
+			case LDD:
+				break;
+			case LDI:
+				this.state.setReg(ir.r1(), ir.param());
+				break;
+			// TODO.
+			case LDX:
+				break;
+			case MOVE:
+				this.state.setReg(ir.r1(), r2);
+				break;
+			case MULT:
+				int mult = r1 * r2;
+
+				if (!isOverflow(mult)) {
+					this.state.setReg(r1, mult);
+				}
+
+				break;
+			// TODO.
+			case STD:
+				break;
+			case STOP:
+				this.state.setIrpt(Interrupt.STOP);
+				break;
+			// TODO.
+			case STX:
+				break;
+			case SUB:
+				int sub = r1 - r2;
+
+				if (!isOverflow(sub)) {
+					this.state.setReg(r1, sub);
+				}
+
+				break;
+			case SUBI:
+				int subI = r1 + ir.param();
+
+				if (!isOverflow(subI)) {
+					this.state.setReg(r1, subI);
+				}
+
+				break;
+			case TRAP:
+				this.syscallHandler.handle(this.state);
+				break;
+			default:
+				this.state.setIrpt(Interrupt.INVALID_INSTRUCTION);
+				break;
+		}
+
+		if (!jump) {
+			this.state.incPc();
+		}
+	}
+
+	private void interrupt() {
+		if (this.state.getIrpt() != null) {
+			interruptHandler.handle(this.state);
+		}
+	}
+
+	private boolean isOverflow(int v) {
 		if (v < Cpu.MIN_INT || v > Cpu.MAX_INT) {
 			this.state.setIrpt(Interrupt.OVERFLOW);
 			return true;
@@ -12,207 +189,15 @@ public class Cpu {
 		return false;
 	}
 
-	private boolean legal(int e) {
-		if(e<0 || e>memory.MEMORY_SIZE){
+	private boolean isLegalAddress(int addr) {
+		if (
+			addr < this.state.getMemoryBase() ||
+			addr > this.state.getMemoryLimit()
+		) {
+			this.state.setIrpt(Interrupt.INVALID_ADDRESS);
 			return false;
 		}
+
 		return true;
 	}
-
-	public void run() {
-		while (true) {
-			if (legal(pc)) {
-				ir = memory.get(pc);
-				if (debugMode) { System.out.print("                               pc: "+pc+"       exec: ");  mem.dump(ir); }
-				switch (ir.opcode) {
-
-					case LDI: // Rd ← k
-						registers[ir.r1] = ir.param;
-						pc++;
-						break;
-
-					case LDD:
-						if (legal(ir.param)) {
-						   registers[ir.r1] = memory.get(ir.param).param;
-						   pc++;
-						}
-						break;
-
-					case LDX:
-						if (legal(registers[ir.r2])) {
-							registers[ir.r1] = memory.get(registers[ir.r2]).param;
-							pc++;
-						}
-						break;
-
-					case STD:
-						if (legal(ir.param)) {
-							memory.get(ir.param).opc = Opcode.DATA;
-							memory.get(ir.param).param = registers[ir.r1];
-							pc++;
-						};
-						break;
-
-					case STX:
-						if (legal(registers[ir.r1])) {
-							memory.get(registers[ir.r1]).opc = Opcode.DATA;
-							memory.get(registers[ir.r1]).param = registers[ir.r2];
-							pc++;
-						};
-						break;
-
-					case MOVE:
-						registers[ir.r1] = registers[ir.r2];
-						pc++;
-						break;
-
-					case ADD:
-						registers[ir.r1] = registers[ir.r1] + registers[ir.r2];
-						testOverflow(registers[ir.r1]);
-						pc++;
-						break;
-
-					case ADDI:
-						registers[ir.r1] = registers[ir.r1] + ir.param;
-						testOverflow(registers[ir.r1]);
-						pc++;
-						break;
-
-					case SUB:
-						registers[ir.r1] = registers[ir.r1] - registers[ir.r2];
-						testOverflow(registers[ir.r1]);
-						pc++;
-						break;
-
-					case SUBI:
-						registers[ir.r1] = registers[ir.r1] - ir.param;
-						testOverflow(registers[ir.r1]);
-						pc++;
-						break;
-
-					case MULT:
-						registers[ir.r1] = registers[ir.r1] * registers[ir.r2];
-						testOverflow(registers[ir.r1]);
-						pc++;
-						break;
-
-					case JMP:
-						pc = ir.param;
-						break;
-
-					case JUPI:
-						pc = ir.r1;
-						break;
-
-					case JMPIG:
-						if (registers[ir.r2] > 0) {
-							pc = registers[ir.r1];
-						} else {
-							pc++;
-						}
-						break;
-
-					case JMPIGK:
-						if (registers[ir.r2] > 0) {
-							pc = ir.param;
-						} else {
-							pc++;
-						}
-						break;
-
-					case JMPILK:
-						 if (registers[ir.r2] < 0) {
-							pc = ir.param;
-						} else {
-							pc++;
-						}
-						break;
-
-					case JMPIEK:
-							if (registers[ir.r2] == 0) {
-								pc = ir.param;
-							} else {
-								pc++;
-							}
-						break;
-
-
-					case JMPIL:
-							 if (registers[ir.r2] < 0) {
-								pc = registers[ir.r1];
-							} else {
-								pc++;
-							}
-						break;
-
-					case JMPIE:
-							 if (registers[ir.r2] == 0) {
-								pc = registers[ir.r1];
-							} else {
-								pc++;
-							}
-						break;
-
-					case JMPIM:
-							 pc = memory.get(ir.param].param;
-						 break;
-
-					case JMPIGM:
-							 if (registers[ir.r2] > 0) {
-								pc = memory.get(ir.param].param;
-							} else {
-								pc++;
-							}
-						 break;
-
-					case JMPILM:
-							 if (registers[ir.r2] < 0) {
-								pc = memory.get(ir.param].param;
-							} else {
-								pc++;
-							}
-						 break;
-
-					case JMPIEM:
-							if (registers[ir.r2] == 0) {
-								pc = memory.get(ir.param].param;
-							} else {
-								pc++;
-							}
-						 break;
-
-					case JMPIGT:
-							if (registers[ir.r1] > registers[ir.r2]) {
-								pc = ir.param;
-							} else {
-								pc++;
-							}
-						 break;
-
-					case STOP:
-						irpt = Interrupts.STOP;
-						break;
-
-					case DATA:
-						irpt = Interrupts.INVALID_INSTRUCTION;
-						break;
-
-					case TRAP:
-						 sysCall.handle();
-						 pc++;
-						 break;
-
-					default:
-						irpt = Interrupts.INVALID_INSTRUCTION;
-						break;
-				}
-			}
-
-			if (!(irpt == Interrupts.noInterrupt)) {
-				ih.handle(irpt,pc);
-				break;
-			}
-		}
-	}
 }
-
